4d0d3b659e131d2abd69ba2d983f547692d31fe1
JesseNReynolds <jessenreynolds@gmail.com>
2021-02-08 14:49:22 -0700

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,45 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+
+    };
+  }
+
+  handleInputChange = (event) => {
+    const key = event.target.name
+    const value = event.target.value
+    this.setState({
+      [key]: value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (this.state.username && this.state.password){
+      this.props.handleLogin(this.state)
+    }
   }
 
   render() {
     return (
-      <form>
+      <form
+      onSubmit={event => this.handleSubmit(event)}
+      >
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" 
+            value={this.state.username}
+            onChange={(event) => this.handleInputChange(event)}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+            value={this.state.password}
+            onChange={(event) => this.handleInputChange(event)}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,26 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: ''
+    };
+  }
+
+  handleContentChange = (event) => {
+    this.setState({
+      content: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message"
+        onChange={(event) => this.handleContentChange(event)}
+        value={this.state.content}
+        />
+        <p> Characters remaining: {this.props.maxChars - this.state.content.length}</p>
       </div>
     );
   }
